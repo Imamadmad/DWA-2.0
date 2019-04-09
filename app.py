d4b01eafaf466a72cdc2968426a311e3cc49dcc2
Maddie J <madeleine.jones@hotmail.com>
2019-04-09 17:10:58 +1000

    'Hello World' example working to serve basic page from file

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-#!/bin/bash python3
-
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__" :
     app.run(port=8000, debug=True)
